06a7de6ce635acd313859132ecad56e546088dd8
Rafael Niebles <rnieblesealo@gmail.com>
2022-03-30 14:11:24 -0400

    Implemented spider, code needs work

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from random import randint
 from pygame import PixelArray, Surface, Rect
 from pygame.math import Vector2
 
+#TODO fix player class & spider class reassigning asset to actual instance of class :o
+
 pygame.init()
 pygame.mixer.init()
 
@@ -53,6 +55,7 @@ road = import_image('assets/road.png', 4)
 shadow = import_image('assets/shadow.png', 3)
 logo = import_image('assets/logo.png')
 game_over = import_image('assets/game_over.png')
+spider = import_image('assets/spider.png', 6)
 
 font = pygame.font.Font('assets/font.ttf', 32) #big version of font
 font_s = pygame.font.Font('assets/font.ttf', 16) #small version of font
@@ -214,8 +217,8 @@ class Player:
     """
 
     texture: Surface = None
-    rect: Rect = None
-    rect_fix = -15 #adjustment applied to rect w and h so its smaller/bigger
+    hitbox: Rect = None
+    hitbox_fix = -15 #adjustment applied to rect w and h so its smaller/bigger
 
     current_lane = 1
     last_direction: Direction = Direction.RIGHT
@@ -236,7 +239,7 @@ class Player:
 
     def __init__(self, texture = player, start_lane = 1) -> None:
         self.texture = texture.copy()
-        self.rect = Rect(0, 0, self.texture.get_width() + self.rect_fix, self.texture.get_height() + self.rect_fix)
+        self.hitbox = Rect(0, 0, self.texture.get_width() + self.hitbox_fix, self.texture.get_height() + self.hitbox_fix)
 
         self.current_lane = start_lane
         self.pos = Vector2(lanes[self.current_lane])
@@ -318,13 +321,13 @@ class Player:
         self.pos = lerp(self.pos, self.__pos, 0.125 * DELTA_TIME * 60)
 
         #update rect (NOTE this isn't used for drawing, it's used for collision!)
-        self.rect.center = self.pos
+        self.hitbox.center = self.pos
 
         #check for collision & gameover event handle
         global state
         if state == GameState.GAME_ON:
             for obstacle in obstacles:
-                if self.rect.colliderect(obstacle.rect):
+                if self.hitbox.colliderect(obstacle.rect):
                     #write new highscore before changing gamestate if we beat our last highscore or none exists
                     if get_highscore() == None or score > get_highscore(in_int=True):
                         with open('save.txt', 'w+') as save:
@@ -349,15 +352,12 @@ class Player:
         
         #set outline colorkey (must do this every time we modify it)
         r_outline.set_colorkey((0, 0, 0))
-        
-        #center rotated outline surface rect
-        r_outline_pos = pos
-        
+                
         #draw outline (we shift it towards every direction to give outline effect)
-        DISPLAY.blit(r_outline, (r_outline_pos[0] - self.outline_width, r_outline_pos[1]))
-        DISPLAY.blit(r_outline, (r_outline_pos[0] + self.outline_width, r_outline_pos[1]))
-        DISPLAY.blit(r_outline, (r_outline_pos[0], r_outline_pos[1] - self.outline_width))
-        DISPLAY.blit(r_outline, (r_outline_pos[0], r_outline_pos[1] + self.outline_width))
+        DISPLAY.blit(r_outline, (pos[0] - self.outline_width, pos[1]))
+        DISPLAY.blit(r_outline, (pos[0] + self.outline_width, pos[1]))
+        DISPLAY.blit(r_outline, (pos[0], pos[1] - self.outline_width))
+        DISPLAY.blit(r_outline, (pos[0], pos[1] + self.outline_width))
 
     def draw(self) -> None:
         # #draw hitbox --NOTE for debugging
@@ -387,6 +387,140 @@ class Player:
         DISPLAY.blit(r_texture, r_texture_rect)
 
 player = Player()
+
+# --- Spider --- 
+
+spider_spawn_ticks = 30 #ticks it takes for spider to peek
+spider_peek_ticks = 10 #ticks it takes for spider to go from peek -> attack
+spider_attack_ticks = 1 #ticks spider takes attacking
+spider_ticks_t = spider_spawn_ticks
+
+class Spider:
+    texture: Surface = None
+    hitbox: Rect = None
+    rect_fix = -15 #adjustment applied to rect w and h so its smaller/bigger
+
+    state = 0 #0 for inactive, 1 for peeking, 2 for active
+    current_lane = 1
+    
+    outline_color = (50, 50, 50)
+    outline_width = 3 #keep below 5
+    
+    pos: Vector2 = None
+
+    y_hidden = 0
+    y_peeked = 0
+    y_attack = 0
+
+    __pos: Vector2 = None
+
+    __outline: Surface = None
+
+    def __init__(self, texture = spider, start_lane = 1) -> None:
+        self.texture = texture.copy()
+        self.hitbox = Rect(0, 0, self.texture.get_width() + self.rect_fix, self.texture.get_height() + self.rect_fix)
+        
+        #set vertical positions for different spider states
+        self.y_hidden = DISPLAY_SIZE[1] + self.texture.get_height() / 2 + 10
+        self.y_peeked = DISPLAY_SIZE[1]
+        self.y_attack = lanes[start_lane].y
+
+        #reset current lane
+        self.current_lane = start_lane
+        
+        #reset position
+        self.pos = Vector2(lanes[self.current_lane].x, self.y_hidden)
+        self.__pos = self.pos.copy()
+
+        #reset state
+        self.state = 0
+
+        #outline generation process
+        
+        #get binary bitmap surface of texture, convert it back to a surface 
+        mask = pygame.mask.from_surface(self.texture)
+        mask_surf = mask.to_surface() #--NOTE the result is a black & white surface where black represents transparent area and white represents filled area (of passed texture)
+        
+        #convert mask_surf into pixel data (numpy array) and replace white with desired color (this will be outline's color)
+        mask_surf_pixels = PixelArray(mask_surf)
+        mask_surf_pixels.replace((255, 255, 255), self.outline_color)
+        
+        self.__outline = mask_surf_pixels.make_surface()
+
+    def reset(self) -> None:                        
+        #reset state
+        self.state = 0
+        
+        #reset position
+        self.pos = Vector2(self.__pos.x, self.y_hidden)
+        self.__pos = self.pos.copy()
+
+        #set random new lane
+        self.current_lane = randint(0, 2)
+
+    #NOTE that drawing the outline is computationally expensive!
+    def draw_outline(self, pos):
+        #rotate outline surface
+        c_outline = self.__outline.copy()
+        
+        #set outline colorkey (must do this every time we modify it)
+        c_outline.set_colorkey((0, 0, 0))
+                
+        #draw outline (we shift it towards every direction to give outline effect)
+        DISPLAY.blit(c_outline, (pos[0] - self.outline_width, pos[1]))
+        DISPLAY.blit(c_outline, (pos[0] + self.outline_width, pos[1]))
+        DISPLAY.blit(c_outline, (pos[0], pos[1] - self.outline_width))
+        DISPLAY.blit(c_outline, (pos[0], pos[1] + self.outline_width))
+
+    def update(self) -> None:       
+        #position spider 
+        global DISPLAY_SIZE
+        match self.state:
+            case 0:
+                self.__pos.y = self.y_hidden
+                pass
+            case 1:
+                self.__pos.y = self.y_peeked
+                pass
+            case 2:
+                self.__pos.y = self.y_attack
+                pass
+            case _:
+                self.__pos.y = self.y_hidden
+
+        self.pos.x = lanes[self.current_lane].x
+
+        self.pos = lerp(self.pos, self.__pos, 0.125 * DELTA_TIME * 60)
+
+        #update rect (NOTE this isn't used for drawing, it's used for collision!)
+        self.hitbox.center = self.pos
+    
+    def draw(self) -> None:
+        # #draw hitbox --NOTE for debugging
+        # pygame.draw.rect(DISPLAY, (0, 255, 0), self.rect)
+                
+        texture_rect = self.texture.get_rect()
+        texture_rect.center = self.pos
+        
+        #draw outline
+        self.draw_outline(texture_rect.topleft)
+
+        #draw texture
+        DISPLAY.blit(self.texture, texture_rect)
+
+spider = Spider()
+
+def spider_time(spider = spider):
+    if spider.state == 0:
+        spider.state = 1
+        spider.current_lane = randint(0, 2)
+        return spider_ticks_t + spider_peek_ticks
+    elif spider.state == 1:
+        spider.state = 2
+        return spider_ticks_t + spider_attack_ticks
+    else:
+        spider.state = 0
+        return spider_ticks_t + spider_spawn_ticks
 
 # GAME LOOP ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -432,19 +566,21 @@ while True:
     DISPLAY.blit(road, road_rect_a)
     DISPLAY.blit(road, road_rect_b)
 
-    #if game not over, update player and draw obstacles
     if state != GameState.GAME_OVER:
+        #if game is not over update player and spider
         player.update()
+        spider.update()
+        
 
     #always draw obstacles in obstacles list
     for obstacle in obstacles:
         obstacle.draw()
     
-    #always draw player
+    #always draw player and spider
     player.draw()
-    
-    if state == GameState.IDLE:            
-        #clear obstacles list when idle
+    spider.draw()
+
+    if state == GameState.IDLE:
         if len(obstacles) != 0:
             obstacles.clear()
 
@@ -452,6 +588,9 @@ while True:
         logo_rect = logo.get_rect()
         logo_rect.center = (DISPLAY_SIZE[0] / 2, 150)
         
+        #reset spider, happens only once
+        spider.reset()
+
         DISPLAY.blit(logo, logo_rect)
 
         #show highscore if we have one
@@ -516,15 +655,20 @@ while True:
         if ticks == speed_ticks_t:
             speed += speed_incr
             speed_ticks_t += speed_ticks
+        
+        #trigger spider
+        if ticks == spider_ticks_t:
+            spider_ticks_t = spider_time()
 
-    #reset values after death
     else:
+        #reset values when game is not on
         ticks = 0
         score = 0
         speed = base_speed
         spawn_ticks_t = spawn_ticks
         score_ticks_t = score_ticks
         speed_ticks_t = speed_ticks
+        spider_ticks_t = spider_spawn_ticks
 
     if state == GameState.GAME_OVER:
         #draw game over
